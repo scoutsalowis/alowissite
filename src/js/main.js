b7fda5e12d9a6adf6deb730adf5891e18e866550
Ben Arts <Ben.Arts@student.hogent.be>
2021-08-30 17:27:43 +0200

    made takken page

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,3 @@
-console.log("start");
-
 const init = function ()
 {
     // close nav when click outside on mobile
@@ -7,23 +5,18 @@ const init = function ()
     const nav_checkbox = document.getElementById("nav-toggle");
     const toggle_label = document.getElementById("toggle-label");
 
-    console.log("ready");
     document.onclick = (ev) =>
     {
-        console.log("click");
         if(!nav_checkbox.checked && toggle_label === ev.target)
         {
-            console.log("box was unchecked + close");
             nav_checkbox.checked = true;
         }
         else if(nav_checkbox.checked && toggle_label === ev.target)
         {
-            console.log("box was checked");
             nav_checkbox.checked = false;
         }
         else if(nav_menu !== ev.target && !nav_menu.contains(ev.target))
         {
-            console.log("close");
             nav_checkbox.checked = false;
         }
     }
